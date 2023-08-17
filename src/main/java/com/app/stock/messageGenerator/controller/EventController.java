65e7cd0826f082d89abe969efb1a3cbd744e6267
AlekseiKalashnik <id50538996@gmail.com>
2023-08-17 21:07:15 +0300

    first attempt to push

--- src/main/java/com/app/stock/messageGenerator/controller/EventController.java
+++ src/main/java/com/app/stock/messageGenerator/controller/EventController.java
@@ -1,13 +1,14 @@
 package com.app.stock.messageGenerator.controller;
 
-import com.app.stock.messageGenerator.entity.Agent;
 import com.app.stock.messageGenerator.job.ProcessMessage;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("api/v1")
@@ -15,7 +16,9 @@ public class EventController {
     private final ProcessMessage processMessage;
 
     @GetMapping("/send/{numberOfAgents}/{messagesPerMinute}")
-    public void createMessages(@PathVariable Integer numberOfAgents, @PathVariable Integer messagesPerMinute) {
-        processMessage.sendMessages(numberOfAgents, messagesPerMinute);
+    public String createMessages(@PathVariable Integer numberOfAgents, @PathVariable Integer messagesPerMinute) {
+        processMessage.sendMessages(numberOfAgents, messagesPerMinute).join();
+        log.info("messages have sent");
+        return "Start to send messages...";
     }
 }
