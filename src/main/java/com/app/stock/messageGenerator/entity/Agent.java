552d59eac6ed93fc4897efaa500c758763dd5f95
AlekseiKalashnik <id50538996@gmail.com>
2023-08-31 11:31:09 +0300

    optimise JsonProperty annotations

--- src/main/java/com/app/stock/messageGenerator/entity/Agent.java
+++ src/main/java/com/app/stock/messageGenerator/entity/Agent.java
@@ -1,17 +1,17 @@
 package com.app.stock.messageGenerator.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
 @JsonIgnoreProperties(ignoreUnknown = true)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class Agent {
-    @JsonProperty(value = "uuid")
     private String UUID;
-    @JsonProperty(value = "agent_id")
     private Gadget agentId;
     private Manufacturer manufacturer;
     private OS os;
--- src/main/java/com/app/stock/messageGenerator/entity/TelemetryMessage.java
+++ src/main/java/com/app/stock/messageGenerator/entity/TelemetryMessage.java
@@ -1,7 +1,8 @@
 package com.app.stock.messageGenerator.entity;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Builder;
 import lombok.Data;
 
@@ -10,17 +11,12 @@ import java.util.List;
 @Data
 @Builder
 @JsonIgnoreProperties(ignoreUnknown = true)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class TelemetryMessage {
-    @JsonProperty(value = "uuid")
     private String UUID;
-    @JsonProperty(value = "agent_id")
     private Gadget agentId;
-    @JsonProperty(value = "previous_message_time")
     private Long previousMessageTime;
-    @JsonProperty(value = "active_service")
     private ActiveService activeService;
-    @JsonProperty(value = "quality_score")
     private Integer qualityScore;
-    @JsonProperty(value = "agents")
     private List<Agent> agents;
 }
