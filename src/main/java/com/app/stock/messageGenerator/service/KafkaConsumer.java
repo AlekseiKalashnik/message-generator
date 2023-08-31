eb23d558e28da65392964e13813dd49fa6f8ec9d
AlekseiKalashnik <id50538996@gmail.com>
2023-08-31 17:53:18 +0300

    test deploy

--- src/main/java/com/app/stock/messageGenerator/service/KafkaConsumer.java
+++ src/main/java/com/app/stock/messageGenerator/service/KafkaConsumer.java
@@ -1,26 +1,26 @@
-package com.app.stock.messageGenerator.service;
-
-import com.app.stock.messageGenerator.entity.TelemetryMessage;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.common.protocol.types.Field;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.stereotype.Service;
-
-import java.util.concurrent.CountDownLatch;
-
-@Slf4j
-@Getter
-@Service
-@RequiredArgsConstructor
-public class KafkaConsumer {
-
-    private String payload;
-
-    @KafkaListener(topics = "${topic.name}", groupId = "${spring.kafka.consumer.group-id}")
-    public void getMessageFromTopic(TelemetryMessage message) {
-        log.info("kafka message consumed='{}'", message.toString());
-        payload = message.toString();
-    }
-}
+//package com.app.stock.messageGenerator.service;
+//
+//import com.app.stock.messageGenerator.entity.TelemetryMessage;
+//import lombok.Getter;
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.apache.kafka.common.protocol.types.Field;
+//import org.springframework.kafka.annotation.KafkaListener;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.concurrent.CountDownLatch;
+//
+//@Slf4j
+//@Getter
+//@Service
+//@RequiredArgsConstructor
+//public class KafkaConsumer {
+//
+//    private String payload;
+//
+//    @KafkaListener(topics = "${topic.name}", groupId = "${spring.kafka.consumer.group-id}")
+//    public void getMessageFromTopic(TelemetryMessage message) {
+//        log.info("kafka message consumed='{}'", message.toString());
+//        payload = message.toString();
+//    }
+//}
--- src/test/java/com/app/stock/messageGenerator/service/KafkaTestcontainersTest.java
+++ src/test/java/com/app/stock/messageGenerator/service/KafkaTestcontainersTest.java
@@ -1,55 +1,55 @@
-package com.app.stock.messageGenerator.service;
-
-import com.app.stock.messageGenerator.entity.Gadget;
-import com.app.stock.messageGenerator.entity.TelemetryMessage;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.containers.KafkaContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
-import org.testcontainers.utility.DockerImageName;
-
-import java.util.UUID;
-
-@SpringBootTest
-@Testcontainers
-class KafkaTestcontainersTest {
-
-    @Container
-    static KafkaContainer kafka = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.3.3"));
-
-    @DynamicPropertySource
-    static void overrideProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.kafka.bootstrap-servers", kafka::getBootstrapServers);
-    }
-
-    @Autowired
-    KafkaTemplate<String, TelemetryMessage> kafkaTemplate;
-
-    @Autowired
-    KafkaConsumer consumer;
-
-    @Value("${topic.name}")
-    private String topic;
-
-    //before test run docker-compose file
-    @Test
-    void testProduceAndConsumeKafkaMessage() {
-        TelemetryMessage message = TelemetryMessage.builder()
-                .UUID(UUID.randomUUID().toString())
-                .agentId(Gadget.getRandom())
-                .build();
-
-        kafkaTemplate.send(topic, message);
-
-        consumer.getMessageFromTopic(message);
-
-        Assertions.assertEquals(consumer.getPayload(), message.toString());
-    }
-}
+//package com.app.stock.messageGenerator.service;
+//
+//import com.app.stock.messageGenerator.entity.Gadget;
+//import com.app.stock.messageGenerator.entity.TelemetryMessage;
+//import org.junit.jupiter.api.Assertions;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.kafka.core.KafkaTemplate;
+//import org.springframework.test.context.DynamicPropertyRegistry;
+//import org.springframework.test.context.DynamicPropertySource;
+//import org.testcontainers.containers.KafkaContainer;
+//import org.testcontainers.junit.jupiter.Container;
+//import org.testcontainers.junit.jupiter.Testcontainers;
+//import org.testcontainers.utility.DockerImageName;
+//
+//import java.util.UUID;
+//
+//@SpringBootTest
+//@Testcontainers
+//class KafkaTestcontainersTest {
+//
+//    @Container
+//    static KafkaContainer kafka = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.3.3"));
+//
+//    @DynamicPropertySource
+//    static void overrideProperties(DynamicPropertyRegistry registry) {
+//        registry.add("spring.kafka.bootstrap-servers", kafka::getBootstrapServers);
+//    }
+//
+//    @Autowired
+//    KafkaTemplate<String, TelemetryMessage> kafkaTemplate;
+//
+//    @Autowired
+//    KafkaConsumer consumer;
+//
+//    @Value("${topic.name}")
+//    private String topic;
+//
+//    //before test run docker-compose file
+//    @Test
+//    void testProduceAndConsumeKafkaMessage() {
+//        TelemetryMessage message = TelemetryMessage.builder()
+//                .UUID(UUID.randomUUID().toString())
+//                .agentId(Gadget.getRandom())
+//                .build();
+//
+//        kafkaTemplate.send(topic, message);
+//
+//        consumer.getMessageFromTopic(message);
+//
+//        Assertions.assertEquals(consumer.getPayload(), message.toString());
+//    }
+//}
